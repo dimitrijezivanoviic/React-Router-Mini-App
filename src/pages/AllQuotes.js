a2c39e78cc1d8c74b96e61aa9e3f3a04de4312d5
Dimitrije <zivanovic2601@gmail.com>
2022-01-04 23:55:25 +0100

    Commit

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Dimitrije", text: "Learning React is fun!" },
+  { id: "q2", author: "Zivanovic", text: "Learning React is greate!" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Pages</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
